bd57103037c2b53538eac0e1cbc910fdec522963
Jake Lee <visiodeibc@naver.com>
2019-07-25 17:48:22 +0800

    container

--- lib/main.dart
+++ lib/main.dart
@@ -11,104 +11,38 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              pictureRow,
-              Column(
-                children: [ratings, iconList],
-              ),
-            ],
-          ),
-        ),
+        body: Center(child: _buildImageColumn()),
       ),
     );
   }
 }
 
-final pictureRow =  Row(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Expanded(
-        child: Image.asset('assets/oasis.png'),
+Widget _buildImageColumn() => Container(
+      decoration: BoxDecoration(
+        color: Colors.black26,
       ),
-      Expanded(
-        child: Image.asset('assets/google.jpg'),
+      child: Column(
+        children: [
+          _buildImageRow(1),
+          _buildImageRow(3),
+        ],
       ),
-      Expanded(
-        child: Image.asset('assets/practice.png'),
-      ),
-    ],
-);
-var star = Row(
-  mainAxisSize: MainAxisSize.max,
-  children: [
-    Icon(Icons.star, color: Colors.green[500]),
-    Icon(Icons.star, color: Colors.green[500]),
-    Icon(Icons.star, color: Colors.green[500]),
-    Icon(Icons.star, color: Colors.green[500]),
-    Icon(Icons.star, color: Colors.green[500]),
-  ],
-);
-final ratings = Container(
-  padding: EdgeInsets.all(20),
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      star,
-      Text(
-        '170 Reviews',
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.w800,
-          fontFamily: 'Roboto',
-          letterSpacing: 0.5,
-          fontSize: 20,
+    );
+
+Widget _buildDecoratedImage(int imageIndex) => Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 10, color: Colors.black38),
+          borderRadius: const BorderRadius.all(const Radius.circular(8)),
         ),
+        margin: const EdgeInsets.all(4),
+        child: Image.asset('assets/pic$imageIndex.jpg'),
       ),
-    ],
-  ),
-);
-final descTextStyle = TextStyle(
-  color: Colors.black26,
-  fontWeight: FontWeight.w500,
-  letterSpacing: 0.5,
-  fontSize: 18,
-  fontFamily: 'Roboto',
-  height: 2,
-);
-final iconList = DefaultTextStyle.merge(
-  style: descTextStyle,
-  child: Container(
-    padding: EdgeInsets.all(20),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    );
+
+Widget _buildImageRow(int imageIndex) => Row(
       children: [
-        Column(
-          children: [
-            Icon(Icons.kitchen, color: Colors.green[500]),
-            Text('PREP: '),
-            Text('25 min'),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(Icons.timer, color: Colors.green[500]),
-            Text('COOK: '),
-            Text('1 hr'),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(Icons.restaurant, color: Colors.green[500]),
-            Text('FEEDS: '),
-            Text('4-6'),
-          ],
-        ),
+        _buildDecoratedImage(imageIndex),
+        _buildDecoratedImage(imageIndex + 1),
       ],
-    ),
-  ),
-);
+    );
