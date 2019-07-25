ba9d3c2b713bc5c7c8e6de69645213c673b45a6b
Jake Lee <visiodeibc@naver.com>
2019-07-25 16:40:57 +0800

    rows_column_mix v2

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,13 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Image.asset('assets/oasis.png'),
-                  ),
-                  Expanded(
-                    child: Image.asset('assets/google.jpg'),
-                  ),
-                  Expanded(
-                    child: Image.asset('assets/practice.png'),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: ratings,
+              pictureRow,
+              Column(
+                children: [ratings, iconList],
               ),
             ],
           ),
@@ -40,6 +29,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final pictureRow =  Row(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Expanded(
+        child: Image.asset('assets/oasis.png'),
+      ),
+      Expanded(
+        child: Image.asset('assets/google.jpg'),
+      ),
+      Expanded(
+        child: Image.asset('assets/practice.png'),
+      ),
+    ],
+);
 var star = Row(
   mainAxisSize: MainAxisSize.max,
   children: [
@@ -67,5 +70,45 @@ final ratings = Container(
         ),
       ),
     ],
+  ),
+);
+final descTextStyle = TextStyle(
+  color: Colors.black26,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.5,
+  fontSize: 18,
+  fontFamily: 'Roboto',
+  height: 2,
+);
+final iconList = DefaultTextStyle.merge(
+  style: descTextStyle,
+  child: Container(
+    padding: EdgeInsets.all(20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: [
+            Icon(Icons.kitchen, color: Colors.green[500]),
+            Text('PREP: '),
+            Text('25 min'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.timer, color: Colors.green[500]),
+            Text('COOK: '),
+            Text('1 hr'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.restaurant, color: Colors.green[500]),
+            Text('FEEDS: '),
+            Text('4-6'),
+          ],
+        ),
+      ],
+    ),
   ),
 );
